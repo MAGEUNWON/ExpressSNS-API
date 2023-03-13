8fab714b660ea6a80cc5c064bfd14531a6c5c102
mageunwon <geunwon1947@gmail.com>
2023-03-13 17:11:34 +0900

    JWT 토큰 미들웨어로 만들고 토큰 내용 저장

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
   if(req.isAuthenticated()){  //로그인 중이면 req.isAuthenticate()가 true, 그렇지 않으면 false. 로그인 여부를 이 메서드로 파악. 
     next();
@@ -13,5 +15,23 @@ exports.isNotLoggedIn = (req, res, next) => {
   } else {
     const message = encodeURIComponent('로그인한 상태입니다.');
     res.redirect(`/?error=${message}`);
+  }
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch(error) {
+    if (error.name === 'TokenExpiredError') { //유효 기간 초과
+      return res.status(419).json({
+        code: 419,
+        message: '토큰이 만료되었습니다',
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않은 토큰입니다',
+    });
   }
 };
