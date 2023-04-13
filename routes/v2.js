7507178cbe217044dcc9f02eec5fb90e553b1d70
mageunwon <geunwon1947@gmail.com>
2023-04-13 21:58:49 +0900

    CORS 처리하고 요청이 온 도메인이 등록된 도메인이 아니면 CORS 허용 안함

--- routes/v2.js
+++ routes/v2.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const url = require('url');
 
 const {verifyToken, apiLimiter} = require('./middlewares');
 const {Domain, User, Post, Hashtag} = require('../models');
@@ -8,9 +9,19 @@ const {Domain, User, Post, Hashtag} = require('../models');
 
 const router = express.Router();
 
-router.use(cors({
-  credentials: true,
-}));
+router.use(async (req, res, next) => {
+  const domain = await Domain.findOne({
+    where: {host : url.parse(req.get('origin')).host},
+  });
+  if(domain){
+    cors({
+      origin: req.get('origin'),
+      credentials: true,
+    })(req, res, next);
+  }else {
+    next();
+  }
+});
 
 router.post('/token', apiLimiter, async(req, res) => {
   const {clientSecret} = req.body;
@@ -28,6 +39,7 @@ router.post('/token', apiLimiter, async(req, res) => {
         message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
       });
     }
+
     const token = jwt.sign({
       id: domain.User.id,
       nick: domain.User.nick,
@@ -53,22 +65,21 @@ router.get('/test', verifyToken, apiLimiter, (req, res)=>{
   res.json(req.docoded);
 });
 
-router.get('posts/my', apiLimiter, verifyToken, (req, res) => {
-  Post.findAll({where:{userID: req.decoded.id}})
-  .then((posts)=>{
+router.get('posts/my', apiLimiter, verifyToken, async (req, res) => {
+  try{
+    const posts = await Post.findAll({where:{userID: req.decoded.id}});
     console.log(posts);
-    res.json({
-      code:200,
-      payload:posts,
+    return res.json({
+      code: 200,
+      payload: posts,
     });
-  })
-  .catch((error) => {
+  } catch (error){
     console.error(error);
     return res.status(500).json({
       code: 500,
       message: '서버 에러',
     });
-  });
+  }
 });
 
 router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async(req, res) => {
@@ -80,6 +91,7 @@ router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async(req, res) => 
         message:'검색 결과가 없습니다',
       });
     }
+
     const posts = await hashtag.getPosts();
     return res.json({
       code:200,
